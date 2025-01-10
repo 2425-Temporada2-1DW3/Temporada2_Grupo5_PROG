253cf2787636525dfaa70f9d59cefa6b6af2a791
lordlichi2006 <Lordlichi2006@gmail.com>
2025-01-10 09:47:14 +0100

    Login y Base de Main
    Añadida logica de Login y Tipo de usuario en main

--- src/com/structure/login.java
+++ src/com/structure/login.java
@@ -5,6 +5,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import com.logic.Usuario;
+
 import java.awt.BorderLayout;
 import javax.swing.JLabel;
 import javax.swing.JButton;
@@ -32,14 +35,22 @@ public class login extends JFrame implements ActionListener {
     private JPanel panel_6;
     private JPanel panel_1;
     private JLabel imgLogo;
-    private JLabel lblPassword;
+    private JLabel lblPass;
     private JLabel lblUser;
     private JLabel lblTitulo;
     private JLabel lblError;
     private JTextField txtUser;
-    private JPasswordField passwordField;
+    private JPasswordField txtPass;
     private JButton btnLogin;
-
+    private Usuario[] users = 	{
+    		new Usuario("Usuario", 0, "Usuario"), 
+    		new Usuario("Arbitro",1,"Arbitro") , 
+    		new Usuario("Admin",2,"Admin")  
+    };
+    private int userType;
+    
+    
+    
     /**
      * Launch the application.
      */
@@ -90,16 +101,16 @@ public class login extends JFrame implements ActionListener {
         panel_2.add(panel_3, BorderLayout.SOUTH);
         panel_3.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 
-        lblPassword = new JLabel("Contraseña");
-        lblPassword.setForeground(new Color(220, 220, 220));
-        lblPassword.setFont(new Font("SansSerif", Font.PLAIN, 15));
-        panel_3.add(lblPassword);
+        lblPass = new JLabel("Contraseña");
+        lblPass.setForeground(new Color(220, 220, 220));
+        lblPass.setFont(new Font("SansSerif", Font.PLAIN, 15));
+        panel_3.add(lblPass);
 
-        passwordField = new JPasswordField();
-        passwordField.setForeground(new Color(220, 220, 220));
-        passwordField.setBackground(new Color(50, 50, 50));
-        passwordField.setColumns(15);
-        panel_3.add(passwordField);
+        txtPass = new JPasswordField();
+        txtPass.setForeground(new Color(220, 220, 220));
+        txtPass.setBackground(new Color(50, 50, 50));
+        txtPass.setColumns(15);
+        panel_3.add(txtPass);
 
         panel_4 = new JPanel();
         panel_4.setBackground(new Color(50, 50, 50));
@@ -130,16 +141,16 @@ public class login extends JFrame implements ActionListener {
         lblTitulo.setForeground(new Color(0, 108, 128));
         lblTitulo.setFont(new Font("Century Gothic", Font.BOLD, 24));
         panel_6.add(lblTitulo);
+        
+        lblError = new JLabel();
+        panel_4.add(lblError, BorderLayout.CENTER);
+        lblError.setForeground(new Color(255, 53, 53));
+        lblError.setFont(new Font("Consolas", Font.PLAIN, 11));
 
         panel_1 = new JPanel();
         panel_1.setBackground(new Color(50, 50, 50));
         contentPane.add(panel_1, BorderLayout.SOUTH);
         panel_1.setLayout(new BorderLayout(0, 0));
-
-        lblError = new JLabel("New label");
-        lblError.setForeground(new Color(255, 53, 53));
-        lblError.setFont(new Font("Consolas", Font.PLAIN, 15));
-        panel_1.add(lblError);
 
         btnLogin = new JButton("Continuar");
         btnLogin.addActionListener(this);
@@ -147,25 +158,61 @@ public class login extends JFrame implements ActionListener {
         btnLogin.setForeground(new Color(220, 220, 220));
         btnLogin.setBackground(new Color(50, 50, 50));
         panel_1.add(btnLogin, BorderLayout.EAST);
-    }
+        
+        // Creacion de los tres usuarios
 
+  
+
+    }
+    private boolean userPassCheck(String u, String p) {
+    	
+    	for (int i = 0; i < users.length; i++) {  
+    		
+        	if (u.equals(users[i].getUser()) && p.equals(users[i].getPassword()) ) {
+        		userType = users[i].getType();
+            	return true;
+        	} 
+    	}
+    	return false;
+    }
+    
     @Override
     public void actionPerformed(ActionEvent ae) {
         Object o = ae.getSource();
-        System.out.print("te");
         
-        String user = lblUser.getText();
-        
-        
-        if (o == btnLogin) {
-        	
-        	
-            Point location = getLocation(); // Coje poscion actual
-            main mainFrame = new main(); // Crea el nuevo frame
-            mainFrame.setLocation(location); // hace que el nuevo frame este en la posicion actual del frame antiguo
-            mainFrame.setVisible(true); // muestra el nuevo frame
+        String gui_user = txtUser.getText();
+        char[] gui_pass_char = txtPass.getPassword();
+        String gui_pass = new String(gui_pass_char);
 
-            dispose(); // destroza el frame antiguo (login)
+        if (o == btnLogin) {
+            if (gui_user.isBlank() && gui_pass.isBlank()) { 
+            	
+                lblError.setText("Por favor, introduzca su nombre de usuario y contraseña.");
+                
+            } else if (gui_user.isBlank()) { 
+            	
+                lblError.setText("El campo de nombre de usuario no puede estar vacío.");
+
+            } else if (gui_pass.isBlank()) { 
+            	
+                lblError.setText("El campo de contraseña no puede estar vacío.");
+
+            } else if (userPassCheck(gui_user, gui_pass)) { 
+            	
+                Point location = getLocation(); // Obtener la posición actual
+                main mainFrame = new main(userType); // Crear el nuevo frame
+                mainFrame.setLocation(location); // Posicionar el nuevo frame en la misma ubicación
+                mainFrame.setVisible(true); // Mostrar el nuevo frame
+                dispose(); // Cerrar el frame actual (login)
+
+            } else { 
+            	
+                lblError.setText("Nombre de usuario o contraseña incorrectos.");
+                
+            }
         }
-    }
+
+        	
+        }
+
 }
