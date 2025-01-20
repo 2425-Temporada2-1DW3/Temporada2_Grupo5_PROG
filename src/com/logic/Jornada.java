758ba25491f41657d97face008ed53b2e19c69df
Anthony Quiliche Calderon <anthonyquili.dev@gmail.com>
2025-01-20 02:46:39 +0100

    Se hizo cambios en el Algoritmo Round Robin, y se creo una v2 de GestionTemporada para aprobacion de esta version.

--- src/com/logic/Jornada.java
+++ src/com/logic/Jornada.java
@@ -4,84 +4,99 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Objects;
 
-public class Jornada implements Comparable<Jornada>, Serializable{ /**
-	 * 
-	 */
+public class Jornada implements Comparable<Jornada>, Serializable {
+	
 	private static final long serialVersionUID = 4644242302041775382L;
 
-	//Defino la clase Jornada
-		//Propiedades o Atributos
-		String NumJor;
-		Partido partido;
-		ArrayList<Partido> listPartidos = new ArrayList<>();
+	// Defino la clase Jornada
+	// Propiedades o Atributos
+	private int id_jornada; // Número de la jornada
+	private ArrayList<Partido> listPartidos; // Lista de partidos
 
-		
-		
-		//Constructor por defecto
-		public Jornada() {
-			NumJor="NULL";
-			partido= new Partido();
-			listPartidos.add(partido);
-		}
-		
-	//Contructor copia
-		public Jornada(Jornada j) {
-			this.NumJor= j.NumJor;
-			this.partido= j.partido;
-			partido = new Partido(j.partido);
-			
+	// Constructor por defecto
+	public Jornada() {
+		this.id_jornada = 0;
+		this.listPartidos = new ArrayList<>();
 	}
-		Jornada(String n) {
-			NumJor = n;
-		    
+
+	// Contructor copia
+	public Jornada(Jornada j) {
+		this.id_jornada = j.id_jornada;
+		this.listPartidos = j.listPartidos;
 	}
-		
-		//Getters and setters
-		
-		 public String getNumJor() {
-			return NumJor;
-		}
 
-		public void setNumJor(String numJor) {
-			NumJor = numJor;
-		}
+	// Constructor con parámetros
+	public Jornada(int id, ArrayList<Partido> listPartidos) {
+		this.id_jornada = id;
+		this.listPartidos = listPartidos != null ? listPartidos : new ArrayList<>();
+	}
+	
+	
+	// Getters y Setters
+    public int getid_jornada() {
+        return id_jornada;
+    }
 
-		public Partido getPartido() {
-			return partido;
-		}
+    public void setid_jornada(int id) {
+        this.id_jornada = id;
+    }
 
-		public void setPartido(Partido partido) {
-			this.partido = partido;
-		}
+    public ArrayList<Partido> getListPartidos() {
+        return listPartidos;
+    }
 
-			//ToString
-			public String toString() {
-				return(NumJor + "" + partido );
-			}
-			
-		@Override
-		public int hashCode() {
-			return Objects.hash( partido, NumJor );
-		}
+    public void setListPartidos(ArrayList<Partido> listPartidos) {
+        this.listPartidos = listPartidos;
+    }
 
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				//si es el mismo objeto
-				return true;
-			if (obj == null)
-				// si el objeto no esta creado
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			//Comparo las propiedades
-			Jornada other = (Jornada) obj;
-			return Objects.equals(NumJor, other.NumJor);
-		}
+    
+    
+    
+    // toString
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
 
-		@Override
-		public int compareTo(Jornada o) {
-			// TODO Auto-generated method stub
-			return 0;
-		}
+        // Encabezado de la jornada
+        sb.append("========== Jornada ").append(id_jornada).append(" ==========\n");
+        sb.append("Cantidad de Partidos: ").append(listPartidos.size()).append("\n\n");
+
+        // Mostrar información de cada partido
+        for (int i = 0; i < listPartidos.size(); i++) {
+            Partido partido = listPartidos.get(i);
+            sb.append("Partido ").append(i + 1).append(":\n");
+            sb.append(partido.toString()).append("\n"); // Utiliza el formato detallado del partido
+        }
+
+        return sb.toString();
+    }
+
+
+
+    // hashCode
+    @Override
+    public int hashCode() {
+        return Objects.hash(id_jornada, listPartidos);
+    }
+
+    // equals
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Jornada jornada = (Jornada) obj;
+        return Objects.equals(id_jornada, jornada.id_jornada) &&
+                Objects.equals(listPartidos, jornada.listPartidos);
+    }
+
+    // compareTo
+    @Override
+    public int compareTo(Jornada other) {
+        // Comparar usando el número de jornada (id_jornada)
+        return Integer.compare(this.id_jornada, other.id_jornada);
+    }
+	
+	
+	
+	
 }
--- src/com/logic/Temporada.java
+++ src/com/logic/Temporada.java
@@ -1,15 +1,22 @@
 package com.logic;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class Temporada implements Comparable<Temporada> {
-    private String nombre;
-    private boolean finalizado;
-    private boolean iniciado;
-    private int cantidadEquipos;
-    private int cantidadJornadas;
     private int id_temporada;
+    private String nombre;
+    private boolean iniciado;
+    private boolean finalizado;
+    private int cantidadJornadas;
+    private int cantidadEquipos;
+    private ArrayList<Jornada> listJornadas; // Nuevo atributo
+    private ArrayList<Equipo> listEquipos; // Nuevo atributo
 
+    public int getIdTemporada() {
+		return id_temporada;
+	}
+    
     public String getNombre() {
 		return nombre;
 	}
@@ -50,7 +57,23 @@ public class Temporada implements Comparable<Temporada> {
 	public void setCantidadJornadas(int cantidadJornadas) {
 		this.cantidadJornadas = cantidadJornadas;
 	}
+	
+	// Getters y Setters para listJornadas
+    public ArrayList<Jornada> getListJornadas() {
+        return listJornadas;
+    }
 
+    public void setListJornadas(ArrayList<Jornada> listJornadas) {
+        this.listJornadas = listJornadas;
+    }
+    
+    public ArrayList<Equipo> getListEquipos() {
+        return listEquipos;
+    }
+
+    public void setListEquipos(ArrayList<Equipo> listEquipos) {
+        this.listEquipos = listEquipos;
+    }
 	public Temporada() {
         nombre = "Temporada Por defecto";
         finalizado = false;
@@ -58,26 +81,29 @@ public class Temporada implements Comparable<Temporada> {
         cantidadEquipos = 6;
         cantidadJornadas = 10;
         id_temporada = 0;
+        listJornadas = new ArrayList<Jornada>(); // Inicializar la lista de jornadas
     }
 
     // Copia
-    public Temporada(Temporada r) {
+    public Temporada(Temporada r) {	
         nombre = r.nombre;
         finalizado = r.finalizado;
         iniciado = r.iniciado;
         cantidadEquipos = r.cantidadEquipos;
         cantidadJornadas = r.cantidadJornadas;
         id_temporada = r.id_temporada;
+        listJornadas = new ArrayList<>(r.listJornadas); // Copiar las jornadas
     }
 
     // Personalizado
-    public Temporada(int id, String nom, int cantEq, int cantJor) {
+    public Temporada(int id, String nom, int cantEq) {
         id_temporada = id;
         nombre = nom;
         cantidadEquipos = cantEq;
-        cantidadJornadas = cantJor;
+        cantidadJornadas = 2 * (cantEq - 1);
         iniciado = false;
         finalizado = false;
+        listJornadas = new ArrayList<>(); // Inicializar la lista de jornadas
     }
 
     public void finalizarTemporada() {
@@ -99,21 +125,32 @@ public class Temporada implements Comparable<Temporada> {
 
     @Override
     public String toString() {
-    	String string = " ";
-    	
-    	if (finalizado && iniciado) {
-    		string = "Temporada " + nombre + ", finalizado: si"  + ", iniciado: si" 
-                    + ", cantidad Equipos: " + cantidadEquipos + ", cantidadJornadas:" + cantidadJornadas;
-    	} else if(!finalizado && iniciado) {
-    		string = "Temporada " + nombre + ", iniciado: si"  + ", finalizado: no"  
-                    + ", cantidadEquipos: " + cantidadEquipos + ", cantidadJornadas:" + cantidadJornadas;
-    	}else if(!finalizado && !iniciado) {
-    		string = "Temporada " + nombre + ", iniciado: no"  + ", finalizado: no"  
-                    + ", cantidadEquipos: " + cantidadEquipos + ", cantidadJornadas:" + cantidadJornadas;
-    	}
-		 return string;
-        
+        StringBuilder sb = new StringBuilder();
+        sb.append("========== Información de la Temporada ==========\n");
+        sb.append("ID Temporada: ").append(id_temporada).append("\n");
+        sb.append("Nombre: ").append(nombre != null ? nombre : "No asignado").append("\n");
+        sb.append("Estado: ").append(iniciado ? (finalizado ? "Finalizado" : "En curso") : "No iniciado").append("\n");
+        sb.append("Cantidad de Equipos: ").append(cantidadEquipos).append("\n");
+        sb.append("Cantidad de Jornadas: ").append(cantidadJornadas).append("\n");
+     // Calcular el total de partidos recorriendo las jornadas
+        int totalPartidos = 0;
+        if (listJornadas != null) {
+            for (Jornada jornada : listJornadas) {
+                if (jornada.getListPartidos() != null) {
+                    totalPartidos += jornada.getListPartidos().size();
+                }
+            }
+        }
+        sb.append("Cantidad de Partidos: ").append(totalPartidos).append("\n");
+
+        sb.append("1-------------------------------------------\n");
+        for (Jornada jornada : listJornadas) {
+            sb.append(jornada.toString()).append("\n");
+        }
+        sb.append("############################################\n");
+        return sb.toString();
     }
+
 
     // Equals & hashCode
     @Override
@@ -130,7 +167,8 @@ public class Temporada implements Comparable<Temporada> {
         if (getClass() != obj.getClass())
             return false;
         Temporada other = (Temporada) obj;
-        return id_temporada == other.id_temporada;
+        return id_temporada == other.id_temporada &&
+                Objects.equals(listJornadas, other.listJornadas);
     }
 
     // compareTo
